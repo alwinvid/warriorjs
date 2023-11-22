9bbdaecab2a37bdd2634ca79ae8ab08a9fcfba37
Alwin Antony <alwin@vidyalai.com>
2023-11-22 14:41:09 +0530

    level 4 finish

--- alwiii-baby-steps/Player.js
+++ alwiii-baby-steps/Player.js
@@ -1,9 +1,19 @@
 class Player {
+  Player() {
+    this.health = 0;
+  }
   playTurn(warrior) {
     if (warrior.feel().isEmpty()) {
-      if (warrior.health() < 10) return warrior.rest();
+      if (warrior.health() < 15) {
+        if (warrior.health() < this.health) {
+          warrior.think("Oh no, i am losing health");
+          warrior.walk();
+        } else warrior.rest();
+      } else warrior.walk();
+      // this.health = warrior.health();
+    } else if (!warrior.feel().isEmpty()) {
+      warrior.attack();
     }
-    if (!warrior.feel().isEmpty()) return warrior.attack();
-    warrior.walk();
+    this.health = warrior.health();
   }
 }
