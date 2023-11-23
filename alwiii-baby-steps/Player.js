7bb578fd21ddb6ce6a4c73741163101553cfb205
Alwin Antony <alwin@vidyalai.com>
2023-11-23 16:33:06 +0530

    finished level 6

--- alwiii-baby-steps/Player.js
+++ alwiii-baby-steps/Player.js
@@ -1,19 +1,46 @@
 class Player {
-  Player() {
+  constructor() {
     this.health = 0;
+    this.direction = "forward";
   }
+
+  //main function
   playTurn(warrior) {
-    if (warrior.feel().isEmpty()) {
-      if (warrior.health() < 15) {
-        if (warrior.health() < this.health) {
-          warrior.think("Oh no, i am losing health");
-          warrior.walk();
-        } else warrior.rest();
-      } else warrior.walk();
-    } else if (!warrior.feel().isEmpty()) {
-      if (warrior.feel().getUnit().isEnemy()) warrior.attack();
-      else if (warrior.feel().getUnit().isBound()) warrior.rescue();
+    if (warrior.feel(this.direction).isEmpty()) {
+      this.handleEmptySpace(warrior);
+    } else {
+      this.handleOccupiedSpace(warrior);
     }
+
     this.health = warrior.health();
+  }
+
+  handleEmptySpace(warrior) {
+    if (warrior.health() < 20) {
+      this.handleLowHealth(warrior);
+    } else {
+      warrior.walk(this.direction);
+    }
+  }
+  handleLowHealth(warrior) {
+    if (warrior.health() < this.health) {
+      warrior.think("Oh no, I am losing health");
+      if (warrior.health() > 10) warrior.walk();
+      else warrior.walk("backward");
+    } else {
+      warrior.rest();
+    }
+  }
+  handleOccupiedSpace(warrior) {
+    const space = warrior.feel(this.direction);
+
+    if (space.isWall()) {
+      this.direction = "forward";
+    } else if (space.getUnit().isEnemy()) {
+      warrior.attack();
+    } else if (space.getUnit().isBound()) {
+      warrior.rescue(this.direction);
+      this.direction = "forward";
+    }
   }
 }
